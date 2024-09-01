b114967e82e5218e07ed28bd9992ba9883448ddf
NeekoKun <leonardo.riccim@gmail.com>
2024-09-01 17:29:10 +0200

    Upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random
+import matplotlib
+
+# Generate polygonal tiling
+#   Random sites
+
+points = np.random.unform()
+#   Calculate Delaunay Triangles
+
+delaunay_trangles = []
+
+
+#   Generate Voronoi Noise
+#   Apply Lloyd's Relaxation
+
+# Separate tiling into plaques
+# Apply force vector to plaques
